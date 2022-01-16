2310013da334e87366ee5276c722c5b61989649d
Douglas Santos <hiperhavy_doug@hotmail.com>
2022-01-15 22:44:50 -0300

    feat: lay eggs

--- card.go
+++ card.go
@@ -1,5 +1,19 @@
 package main
 
 type Card struct {
-    name string
+	name      string
+	eggs      uint8
+	eggsLimit uint8
+}
+
+func (card *Card) CountEggs() uint8 {
+    return card.eggs
+}
+
+func (card *Card) LayEggs(qty uint8) {
+    for i := uint8(0); i < qty; i++ {
+        if card.eggs < card.eggsLimit {
+            card.eggs++
+        }
+    }
 }
---
+++ lay_eggs_test.go
@@ -0,0 +1,21 @@
+package main
+
+import "testing"
+
+func TestBirdLaysEggs(t *testing.T) {
+    card := &Card{eggsLimit: 4}
+	card.LayEggs(2)
+
+	if card.CountEggs() != 2 {
+		t.Errorf("Expected card to have 2 eggs, but got %d", card.CountEggs())
+	}
+}
+
+func TestEggsLimitIsRespected(t *testing.T) {
+    card := &Card{eggsLimit: 2}
+	card.LayEggs(4)
+
+	if card.CountEggs() != 2 {
+		t.Errorf("Expected card to have 2 eggs, but got %d", card.CountEggs())
+	}
+}
